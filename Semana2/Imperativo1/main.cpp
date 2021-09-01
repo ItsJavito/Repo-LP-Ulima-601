ea3a7c20de4403f500f551be3d571de2235ae9e4
Javier <javierenriqueos@gmail.com>
2021-09-01 11:06:25 -0500

    Semana2

---
+++ Semana2/Imperativo1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int edad; // DEFINIENDO variable -> [-2 146 483 648 , 2 146 483 647] 4 294 966 296
+    edad = 10; // ASIGNANDO un valor a la variable
+    int edad1 = 20; // DEFINIENDO y ASIGNANDO un valor a variable
+
+    std::cout << "Edad: " << edad << std::endl; 
+    
+    char letra = 65; 
+    std::cout << "letra: " << letra << std::endl;
+
+    return 0;
+}
